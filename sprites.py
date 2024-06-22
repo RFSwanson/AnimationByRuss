809acc205f535716bbd6ed7c5b5b064d05ec011c
Ron Swanson <swansonofmaine@aol.com>
2024-06-22 05:09:19 -0400

    final version

--- sprites.py
+++ sprites.py
@@ -15,29 +15,53 @@ sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
 BG = (50, 50, 50)
 BLACK = (0, 0, 0)
 
-#scaled by 3 so new sub image is 72 pixels wide
-frame_0 = sprite_sheet.get_image(0, 24, 24, 3, BLACK)
-frame_1 = sprite_sheet.get_image(1, 24, 24, 3, BLACK)
-frame_2 = sprite_sheet.get_image(2, 24, 24, 3, BLACK)
-frame_3 = sprite_sheet.get_image(3, 24, 24, 3, BLACK)
+#create animation list
+animation_list = []
+animation_steps = [4,6,3,4] #each step item is a different kind of image movement
+action = 0 #use this to change code to different types of animation
+last_update = pygame.time.get_ticks()
+animation_cooldown = 75
+frame = 0
+step_counter = 0 #corresponds to the frame number of the sprite sheet
 
+for animation in animation_steps:
+    temp_img_list = []
+    for _ in range(animation):
+        temp_img_list.append(sprite_sheet.get_image(step_counter
+                                                    ,24,24,3,BLACK))
+        step_counter += 1
+    animation_list.append(temp_img_list)
+    
 run = True
 while run:
 
 	#update background
 	screen.fill(BG)
+ 
+	#update animation
+	current_time = pygame.time.get_ticks()
+	if current_time - last_update >= animation_cooldown:
+		frame += 1
+		last_update = current_time
+		if frame >= len(animation_list[action]):
+			frame = 0
+	
 
 	#show frame image
- #get_image gets the image but blit determines coordinate each image will be on screen
-	screen.blit(frame_0, (0, 0))
-	screen.blit(frame_1, (28, 0))
-	screen.blit(frame_2, (56, 0))
-	screen.blit(frame_3, (100, 0))
+	screen.blit(animation_list[action][frame], (0, 0)) #each image scale by 3:(24*3)
+	
 
 	#event handler
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_DOWN and action > 0:
+				action -= 1
+				frame = 0
+			if event.key == pygame.K_UP and action < len(animation_list)-1:
+				action += 1
+				frame = 0
 
 	pygame.display.update()
 
